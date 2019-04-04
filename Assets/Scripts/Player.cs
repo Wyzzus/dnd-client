035fdda5c92ea4d7b0d5acb7ad7a586e8915d025
Wyzzus <wyzzus@gmail.com>
2019-04-05 01:12:27 +0300

    Add suits sprites

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+    public float SlerpTime = 5f;
+    public Vector3 NewPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+        if (Input.GetMouseButton(1))
+            NewPosition = Input.mousePosition;
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(NewPosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(hit.point.x, transform.position.y, hit.point.z), SlerpTime * Time.deltaTime);
+        }
+    }
+}
